b85882d04c2f10d280f4bf53213d12f27d949b5f
Sonawane-Punam <98309601+Sonawane-Punam@users.noreply.github.com>
2022-02-20 18:55:42 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+            <div class="header">
+                <h2>The Spark Foundation Bank</h2>
+            </div>
+        <div class="inner">
+            <div class="main">
+                <ul>
+                    <li>
+                        <a href=" ">Home</a>
+                    </li>
+                    <li>
+                        <a href="createuser.php">Create User</a>
+                    </li>
+                    <li>
+                        <a href="transfermoney.php">Send Money</a>
+                    </li>
+                    <li>
+                        <a href="transactionhistory.php">Transaction History</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </body>
+</html>
